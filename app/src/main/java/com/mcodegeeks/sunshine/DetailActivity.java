e8bd51b8635f1c3d3a9b57ec11b9e4ea11d8f91d
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-06-07 11:30:00 -0400

    launch SettingActivity

--- app/src/main/java/com/mcodegeeks/sunshine/DetailActivity.java
+++ app/src/main/java/com/mcodegeeks/sunshine/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.mcodegeeks.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
@@ -19,6 +22,29 @@ public class DetailActivity extends AppCompatActivity {
                     .commit();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/mcodegeeks/sunshine/MainActivity.java
+++ app/src/main/java/com/mcodegeeks/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mcodegeeks.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
